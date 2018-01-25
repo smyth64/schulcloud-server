01f5897e73957bb8e02e3dded0c7eac6d2acdfa9
dominikglandorf <do.gl@gmx.de>
2018-01-25 11:51:18 +0100

    insert statements to learning locker

--- src/services/lrs/service/index.js
+++ src/services/lrs/service/index.js
@@ -12,9 +12,9 @@ class LearningLockerStore {
 	}
 
 	create(data, params) {
-		// TODO: POST /statements to learning locker
-		const baseUrl = 'http://172.20.8.5/data/xAPI/';
-		const authorization = 'OTJhMDMxYzc4ZDkzMjFmYmEyNDM4Y2QwMjU5NzgzYTU5MDQ0MjU2NzoxZjZhOWNmNTQyNWZjMGQzYjg1YTZjYmZmY2M1OTM1Nzk2Mzg5N2Nl';
+		// TODO: outsource endpoint configuration
+		const baseUrl = 'https://bp.schul-cloud.org/data/xAPI/';
+		const authorization = 'MzJhODhlM2E0YjdkZDE3OThhMjE1YjI3MDE0N2NmNjRkM2IwZGI2YjowNzA0NWUzZTM5OTE0M2M0OTYzOTNkNDRlNzdiODFkY2ViODdlNjEw';
 
 		const options = {
 			method: 'POST',
@@ -23,21 +23,10 @@ class LearningLockerStore {
 				'Authorization': authorization,
 				'X-Experience-API-Version': '1.0.3'
 			},
-			body: [
-				{
-					"actor": { "mbox": "mailto:test2@example.org" },
-					"verb": { "id": "http://www.example.org/verb" },
-					"object": { "id": "http://www.example.org/activity"}
-				},
-				{
-					"actor": { "mbox": "mailto:test2@example.org"},
-					"verb": {"id": "http://adlnet.gov/expapi/verbs/completed"},
-					"object": {"objectType": "Activity", "id": "http://www.example.com/activities/1"}
-				}
-			],
+			body: data,
 			json: true
 		};
-		console.log(options);
+
 		return rp(options).then(result => {
 			return Promise.resolve(result);
 		}).catch(err => {
